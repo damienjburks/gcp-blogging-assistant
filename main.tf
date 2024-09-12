5e49a26efd005119443573565963943db73abef5
Damien Burks <damien@damienjburks.com>
2024-09-12 15:45:49 -0500

    updating

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "google_workflows_workflow" "dsb_blog_assistant_workflow" {
 
   source_contents = <<-EOT
   main:
+    params: [input]
     steps:
     - getVideoInformation:
         call: http.post
@@ -15,76 +16,11 @@ resource "google_workflows_workflow" "dsb_blog_assistant_workflow" {
           body:
             videoName: $${videoName}
             videoUrl: $${videoUrl}
-        result: videoInfo
-    - isVideoShort:
-        switch:
-          - condition: $${videoInfo.isShort}
-            next: ignoreShortVideo
-          - next: sendVideoConfirmationEmail
-
-    - ignoreShortVideo:
-        return: "Video ignored because it is short."
-
-    - sendVideoConfirmationEmail:
-        call: http.post
-        args:
-          url: "${google_cloudfunctions_function.processing_function.https_trigger_url}/sendConfirmationEmail"
-          body:
-            videoName: $${videoName}
-            token: $${sys.task_token}
-            ExecutionContext: $${sys.execution}
-            processorLambdaFunctionUrl: $${YOUR_PROCESSOR_LAMBDA_FUNCTION_URL}
-        result: emailResponse
-
-    - isVideoTechnical:
-        switch:
-          - condition: $${emailResponse.Status == "Video is confirmed as technical!"}
-            next: generateTechnicalBlogPost
-          - next: generateNonTechnicalBlogPost
-
-    - generateTechnicalBlogPost:
-        call: http.post
-        args:
-          url: "${google_cloudfunctions_function.processing_function.https_trigger_url}/generateBlogPost"
-          body:
-            videoName: $${videoName}
-            videoType: "technical"
-            videoId: $${videoInfo.videoId}
-        result: technicalBlogPost
-        next: publishBlogToGitHub
-
-    - generateNonTechnicalBlogPost:
-        call: http.post
-        args:
-          url: "${google_cloudfunctions_function.processing_function.https_trigger_url}/generateBlogPost"
-          body:
-            videoName: $${videoName}
-            videoType: "non-technical"
-            videoId: $${videoInfo.videoId}
-        result: nonTechnicalBlogPost
-        next: publishBlogToGitHub
-
-    - publishBlogToGitHub:
-        call: http.post
-        args:
-          url: "${google_cloudfunctions_function.processing_function.https_trigger_url}/commitBlogToGitHub"
-          body:
-            videoName: $${videoName}
-            blogPostContents: $${technicalBlogPost.blogPostContents or nonTechnicalBlogPost.blogPostContents}
-        result: commitResponse
-        next: sendEmailToDSB
-
-    - sendEmailToDSB:
-        call: http.post
-        args:
-          url: "${google_cloudfunctions_function.processing_function.https_trigger_url}/sendEmail"
-          body:
-            commitId: $${commitResponse.commitId}
-            branchName: $${commitResponse.branchName}
-            videoName: $${videoName}
-        result: emailResult
-        return: emailResult
+    - returnOutput:
+        return: $${getVideoInformation.json}
   EOT
+
+  depends_on = [ google_cloudfunctions_function.processing_function ]
 }
 
 resource "google_service_account" "dsb_blog_assistant_sa" {
@@ -128,15 +64,10 @@ resource "google_storage_bucket_object" "src" {
 
 resource "google_cloudfunctions_function" "processing_function" {
   name                  = "dsb-ba-processor"
-  runtime               = "python312"
+  runtime               = "python310"
   entry_point           = "main"
   source_archive_bucket = google_storage_bucket_object.src.bucket
   source_archive_object = google_storage_bucket_object.src.name
-
-  event_trigger {
-    event_type = "google.storage.object.finalize"
-    resource   = google_storage_bucket.default.name
-  }
-
   https_trigger_security_level = "SECURE_ALWAYS"
+  trigger_http = true
 }
